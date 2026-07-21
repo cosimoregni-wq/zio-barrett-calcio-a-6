c4dc1f4ea82d7910a61c51f4d582625c8100fc12
cosimoregni-wq <cosimo.regni@gmail.com>
2026-07-21 13:29:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from telegram.ext import (
     ContextTypes,
     filters,
 )
+
 import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
 
 MENU = ReplyKeyboardMarkup(
     [
@@ -19,32 +25,58 @@ MENU = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+# Connessione Google Sheets
+credentials_dict = json.loads(GOOGLE_CREDENTIALS)
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    credentials_dict,
+    scopes=scopes
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key(SPREADSHEET_ID)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Benvenuto nel FC Roccaraso Bot!",
+        "⚽ Benvenuto nel Bot del Torneo!",
         reply_markup=MENU
     )
+
+async def mostra_rosa(update: Update):
+    worksheet = sheet.worksheet("ROSA")
+
+    dati = worksheet.get_all_records()
+
+    testo = "👥 ROSA FC ROCCARASO\n\n"
+
+    for giocatore in dati:
+        testo += f"{giocatore['NR']} - {giocatore['NOME']}\n"
+
+    await update.message.reply_text(testo)
 
 async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
-    if text == "🏆 Classifica":
-        await update.message.reply_text("Funzione classifica in preparazione.")
+    if text == "👥 Rosa":
+        await mostra_rosa(update)
+
+    elif text == "🏆 Classifica":
+        await update.message.reply_text("Classifica in sviluppo")
 
     elif text == "📅 Calendario":
-        await update.message.reply_text("Funzione calendario in preparazione.")
+        await update.message.reply_text("Calendario in sviluppo")
 
     elif text == "⚽ Prossima Partita":
-        await update.message.reply_text("Funzione prossima partita in preparazione.")
-
-    elif text == "👥 Rosa":
-        await update.message.reply_text("Funzione rosa in preparazione.")
+        await update.message.reply_text("Prossima partita in sviluppo")
 
     elif text == "🚑 Indisponibili":
-        await update.message.reply_text("Funzione indisponibili in preparazione.")
-
-    else:
-        await update.message.reply_text("Seleziona una voce dal menu.")
+        await update.message.reply_text("Indisponibili in sviluppo")
 
 if __name__ == "__main__":
     app = Application.builder().token(BOT_TOKEN).build()
